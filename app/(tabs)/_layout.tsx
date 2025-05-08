5eafa62e8084df536da52319130f9192a99e377f
Gabriel Moreira Rolim <you@example.com>
2025-05-08 16:17:11 -0300

    bairee-finished

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,20 +1,52 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Tabs } from 'expo-router';
 
-export default function RootLayout() {
-  return (
-    <Tabs
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#f4511e',
+export default function Layout() {
+    return (
+        <Tabs screenOptions={{
+          tabBarStyle:{
+          backgroundColor: "#000",
         },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
+        headerStyle:{
+          backgroundColor: "#000",
         },
-      }}>
-      <Tabs.Screen name="home" />
-      <Tabs.Screen name="travels" />
-      <Tabs.Screen name="settings" />
-    </Tabs>
-  );
+        }}>
+            <Tabs.Screen
+                name="home"
+                options={{
+                title: 'Home',
+                headerTitleStyle: {
+                  color: '#fff'
+              },
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        
+          }}    
+          />
+            
+
+            <Tabs.Screen
+                name="travels"
+                options={{
+                  title: 'Minhas Caronas',
+                  headerTitleStyle: {
+                    color: '#fff'
+                },
+            tabBarIcon: ({ color }) => <FontAwesome size={28} name="map-o" color={color} />,
+          
+            }}    
+                
+            />
+            <Tabs.Screen
+                name="settings"
+                options={{
+                  title: 'Meu Perfil',
+                  headerTitleStyle: {
+                    color: '#fff'
+                },
+                  tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+                }}
+                
+            />
+        </Tabs>
+    );
 }
