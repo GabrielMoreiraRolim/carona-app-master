c1c92f6c4c05040aa09e0eda672fee5bc20ec2f5
Gabriel Moreira Rolim <you@example.com>
2025-05-08 16:28:04 -0300

    travelsstyle-started

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Home = () => {
     return (
         <View>
-            <Text>Home</Text>
+            <Text>TODES SÃO BEM-VINDES!!!</Text>
         </View>
     )
 }
